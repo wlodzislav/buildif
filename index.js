a434bf4e47b8c081568f503ca3315af9fb77de6b
Vladislav Kaminsky <wlodzislav@ya.ru>
2019-01-29 17:29:09 +0400

    Import project

---
+++ index.js
@@ -0,0 +1,23 @@
+function buildif(text, config, _filePath) {
+	var body = [];
+	body.push("var result = []; var lineNo; try { with (config) {");
+	text.split("\n").forEach(function (line, lineNo) {
+		if (line.match(/^\s*\/\/\s*BUILD:/)) {
+			body.push("lineNo = " + (lineNo + 1) + ";" + line.replace(/^\s*\/\/\s*BUILD:/, ""));
+		} else {
+			body.push("lineNo = " + (lineNo + 1) + "; result.push(\"" + line.replace(/"/g, "\\\"") + "\");");
+		}
+	});
+
+	body.push("} } catch (err) { err.lineNo = lineNo; throw err; } return result.join(\"\\n\");");
+	var textBody = body.join("\n");
+	var compiled = new Function("config", textBody);
+
+	return compiled(config);
+
+};
+
+var text = require("fs").readFileSync("examples/example.js", "utf8");
+
+module.exports = buildif;
+
